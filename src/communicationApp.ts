42c00fc7cc282cd175802acd1eb25c3335d4933d
Bob Reselman <bob@cogarttech.com>
2024-03-13 13:53:39 -0700

    updating code

--- src/communicationApp.ts
+++ src/communicationApp.ts
@@ -4,7 +4,7 @@ import {SMSMessenger} from './communication/SMSMessenger';
 
 (async () => {
   try {
-    const numberOfMessages = 5
+    const numberOfMessages = 5;
     const person = Randomizer.getRandomPerson();
 
     const emailMessenger = new EmailMessenger(person);
@@ -13,11 +13,10 @@ import {SMSMessenger} from './communication/SMSMessenger';
     for (let i = 0; i < numberOfMessages; i++) {
       emailMessenger.sendMessage(Randomizer.getRandomMessage());
       smsMessenger.sendMessage(Randomizer.getRandomMessage());
-    };
+    }
 
     console.log({emailMessages: emailMessenger.getMessages()});
     console.log({smsMessages: smsMessenger.getMessages()});
-
   } catch (e: any) {
     console.error(e.message);
   }
