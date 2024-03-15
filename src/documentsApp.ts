b9cdc128d3b75f1184ea36fb7beb8a3bc430c116
Bob Reselman <bob@cogarttech.com>
2024-03-14 20:47:41 -0700

    making docuemnts only

--- src/documentsApp.ts
+++ src/documentsApp.ts
@@ -1,26 +1,23 @@
-import {Randomizer} from './utils/Randomizer';
-import {EmailMessenger} from './communication/EmailMessenger';
-import {SMSMessenger} from './communication/SMSMessenger';
-import {WebDocument} from "./document/WebDocument";
-import {IDocument} from "./document/IDocument";
-import {HardCopyDocument} from "./document/HardCopyDocument";
-import {ILogEntry} from "./document/ILogEntry";
-import {ILogger} from "./document/ILogger";
+import {IDocument} from './document/IDocument';
+import {HardCopyDocument} from './document/HardCopyDocument';
+import {ILogEntry} from './document/ILogEntry';
+import {ILogger} from './document/ILogger';
+import {WebDocument} from './document/WebDocument';
 
 (async () => {
   try {
-    //let document: IDocument;
     let document = new WebDocument('Understanding the World Wide Web');
+    console.log('\n----------------\n');
     (document as IDocument).print();
-
-    let logEntries: Array<ILogEntry> = (document as ILogger).getLogEntries()
-    console.log({webDocumentEntries: logEntries});
+    let logEntries: Array<ILogEntry> = (document as ILogger).getLogEntries();
+    console.log({webDocumentLogEntries: logEntries});
     console.log('\n----------------\n');
 
     document = new HardCopyDocument('Hard Copy Manifesto');
     (document as IDocument).print();
     logEntries = (document as ILogger).getLogEntries();
-    console.log({hardCopyEntries: logEntries});
+    console.log({hardCopyLogEntries: logEntries});
+    console.log('\n----------------\n');
   } catch (e: any) {
     console.error(e.message);
   }
